9063941cf718388b315e48746328299743b0ffb6
tomomik <tomomik210@gmail.com>
2025-09-04 07:14:15 +0900

    4.2.2 テストワークフロー
    ErrorからErrorfに変更

--- go/excellent/main_test.go
+++ go/excellent/main_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestEvenOrOdd(t *testing.T) {
 	result := EvenOrOdd(10)
 	if result != "even" {
-		t.Error("Expected: even, actual: %s", result)
+		t.Errorf("Expected: even, actual: %s", result)
 	}
 }
